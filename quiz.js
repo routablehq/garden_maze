bb0e1ef44b723d78040095693c96b743b626ffd4
Virtim <timvir@gmail.com>
2021-12-08 16:35:09 -0800

    questions

---
+++ quiz.js
@@ -0,0 +1,62 @@
+export const quiz =  {
+  "quizTitle": "React Quiz Component Demo",
+  "quizSynopsis": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim",
+  "questions": [
+    {
+      "question": "Identifies himself as Make of Chaos",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "James",
+        "James",
+        "Justin",
+        "Jake"
+      ],
+      "correctAnswer": "3",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+    },
+    {
+      "question": "Calls football a Handegg?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "Tim ",
+        "Yulyana",
+        "Omri",
+        "Diego"
+      ],
+      "correctAnswer": "1",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Football is when you kick a round ball with a foot. Not when you throw eggshaped object with your hand",
+    },
+    {
+      "question": "Grew up on the island and hates Cilantro",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "Megan Farley",
+        "Justin",
+        "Brian Walerius",
+        "Brian Sweatt"
+      ],
+      "correctAnswer": "1",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+    },
+    {
+      "question": "Had a near miss with a whale shark.",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "Michael Guerrero",
+        "Matt Bruhn",
+        "Both of the above",
+      ],
+      "correctAnswer": "1",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+    },
+  ]
+}
